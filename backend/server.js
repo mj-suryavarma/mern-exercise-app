ae5454f9461aa6cf301c0a5e50351c2a35e7ae99
M-jithendra-suryavarma <suriyamp7550@gmail.com>
2021-11-28 01:11:59 +0530

    bug fixed

--- backend/server.js
+++ backend/server.js
@@ -4,9 +4,8 @@ require('dotenv').config({ path: 'ENV_FILENAME' });
 const express = require('express');
 const app = express()
 
-/// static file
-app.use(express.static(path.resolve(__dirname,'../client/build')))
-const port = process.env.PORT || 5000
+app.use(express.json())
+app.use(express.urlencoded({extended:false}))
 
 //// security
 const cors = require('cors')
@@ -17,14 +16,22 @@ app.options(cors())
 const NotFound = require('./middleware/notfound')
 const errorHandlerMiddleware = require('./middleware/errorhandler')
 const authenticationMiddleware = require('./middleware/authentication')
-
+//// db
+const connectDB = require('./db/connect');
 ///// routes import 
 const userRoute = require('./routes/auth')
 const exerciseRoute = require('./routes/exercise')  
 
+/// static file
+app.use(express.static(path.resolve(__dirname,'../client/build')))
+const port = process.env.PORT || 5000
 
-//// db
-const connectDB = require('./db/connect');
+
+//// routes
+app.use('/api/v1/auth',userRoute);
+
+app.use('/api/v1/exercise',authenticationMiddleware,exerciseRoute);  
+
 
 
 if(process.env.NODE_ENV === 'production'){
@@ -33,31 +40,19 @@ if(process.env.NODE_ENV === 'production'){
 }
 
 
-
-app.use(express.json())
-app.use(express.urlencoded({extended:false}))
-
-
-
-//// routes
-app.use('/api/v1/auth',userRoute);
-
-app.use('/api/v1/exercise',authenticationMiddleware,exerciseRoute);  
-
 /// default route
 app.get('/api',(req,res) => res.json({msg: "hello world from backend"}))
 
 
-
+app.get('*',(req, res) => {
+    res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'));
+});
 
 // use middleware
 app.use(NotFound)
 app.use(errorHandlerMiddleware)
 
 
-app.get('*',(req, res) => {
-    res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'));
-});
 
 
 const start = async () =>{
